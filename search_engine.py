455550d9cac3af1d747fb6c75f92818bc16b6b08
sameer <sameer6n@gmail.com>
2015-04-26 00:01:47 -0500

    Start working on Crawler and parser

---
+++ search_engine.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def downloadUrl(url):
+	r = requests.get(url)
+	if r.status_code!=200:
+		raise Exception("Non-ok status code:{}".format(r.status_code))
+	return r.text
+def parseText(html):
+	bs = BeautifulSoup(html)
+	return bs.select('div.usertext-body')[1].text
+
